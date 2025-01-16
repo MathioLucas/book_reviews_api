d56dfed684cabe465bc3d2bc28dd8092cbc03b07
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2025-01-16 16:25:02 -0500

    Update models.py
    Created book base model

--- models.py
+++ models.py
@@ -1,1 +1,10 @@
+import typing import optional 
+from pydantic import BaseModel, Field
+from datetime import datetime
 
+class book(BaseModel):
+  id: Optional[str]
+  Title: str
+  Author: str
+  Publication_date: datetime
+  
