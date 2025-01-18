379a748b2fcae94c0e36fae78e5fde686d465e25
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2025-01-17 22:05:56 -0500

    Update crud.py

--- crud.py
+++ crud.py
@@ -9,8 +9,15 @@ def book_helper(book) - > dict:
     "Title": book["Title"]
     "Author": book["Author"]
     "Publication_date": book["Publication_date"]
-
-
-
   }
 
+# CRUD function
+async def add_book(book_data: dict) -> dict:
+    book = await book_collection.insert_one(book_data)
+    new_book = await book_collection.find_one({"_id": book.inserted_id})
+    return book_helper(new_book)
+
+async def get_book(id: str) -> dict:
+    book = await book_collection.find_one({"_id": ObjectId(id)})
+    if book:
+        return book_helper(book)
