8ef59cfed0f76c19f4a4438c02167ca15caa792f
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2025-01-17 21:58:59 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,10 @@
-import motor .motor_asyncio
+import motor.motor_asyncio
+
+MONGO_DETAILS = "mongodb://localhost:27017"
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+
+database = client.book_review
+
+book_collection = database.get_collection("books")
+
