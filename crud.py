01dc8c5ff5b84880418af57621f955a5ac5d11d4
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2025-01-17 22:02:55 -0500

    Update crud.py
    book helper function

--- crud.py
+++ crud.py
@@ -3,3 +3,14 @@ from database import book_collection, review_collection
 from datetime import datetime
 from bson import ObjectId
 
+def book_helper(book) - > dict:
+  return {
+    "id": str(book["_id"]),
+    "title"
+    "author"
+    "
+
+
+
+  }
+
